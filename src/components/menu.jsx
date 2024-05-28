3e89baa2591316189ea954b36753b25780808725
Juan P Javier <juanpablojavier14@gmail.com>
2024-05-28 20:03:49 -0300

    download json example template

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,13 +1,38 @@
 import './menu.css'
 export function Menu (){
+
+    const downloadFile = (fileName) => {
+        const filePath = `/src/${fileName}.json`;
+
+        fetch(filePath)
+            .then(response => {
+                if (response.ok) {
+                    return response.blob();
+                } else {
+                    throw new Error('File not found');
+                }
+            })
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = `${fileName}.json`;
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+            })
+            .catch(error => {
+                console.error('Error downloading the file:', error);
+            });
+    };
+
+
     return (
         <div className="containerMenu">
-            <h3 className='headMenu'>Plantillas de ejemplo:
-            </h3>
-
-            <h2>Arte</h2>
-            <h2>Fútbol</h2>
-            <h2>Historia</h2>
+            <h3 className='headMenu'>Plantillas de ejemplo:</h3>
+            <h2 onClick={() => downloadFile('PreguntasArte')}>Arte</h2>
+            <h2 onClick={() => downloadFile('PreguntasFutbol')}>Fútbol</h2>
+            <h2 onClick={() => downloadFile('PreguntasHistoria')}>Historia</h2>
         </div>
     )
 }
